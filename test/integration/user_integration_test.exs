8baaf3d35e53bb667576caf06c29fb32568fd0a9
David Whitlock <alovedalongthe@gmail.com>
2017-09-08 16:33:04 +0700

    Updated integration tests

--- test/integration/user_integration_test.exs
+++ test/integration/user_integration_test.exs
@@ -12,37 +12,36 @@ defmodule TodoAppWeb.UserIntegrationTest do
 
   def login_user(password \\ "mangoes&g0oseberries") do
     body = %{session: %{email: "ted@mail.com", password: password}}
-    Api.post("/sessions", body)
+    Api.post!("/sessions", body).body
   end
 
   test "login and use token to access protected resource" do
-    {:ok, %{body: %{"access_token" => token}}} = login_user()
-    {:ok, %{body: %{"data" => data}}} = Api.get("/users", [{"Authorization", token}])
+    %{"access_token" => token} = login_user()
+    %{"data" => data} = Api.get!("/users", Api.key(token)).body
     assert length(data) == 2
   end
 
   test "login and view other user's details", %{other: %{id: id}} do
-    {:ok, %{body: %{"access_token" => token}}} = login_user()
-    {:ok, %{body: %{"data" => data}}} = Api.get("/users/#{id}", [{"Authorization", token}])
+    %{"access_token" => token} = login_user()
+    %{"data" => data} = Api.get!("/users/#{id}", Api.key(token)).body
     assert data["id"] == id
   end
 
   test "login fails for incorrect password" do
-    {:ok, %{body: %{"errors" => %{"detail" => message}}}} = login_user("password")
+    %{"errors" => %{"detail" => message}} = login_user("password")
     assert message =~ "need to login"
   end
 
   test "can delete own user", %{user: %{id: id}} do
-    {:ok, %{body: %{"access_token" => token}}} = login_user()
-    {:ok, response} = Api.delete("/users/#{id}", [{"Authorization", token}])
+    %{"access_token" => token} = login_user()
+    response = Api.delete!("/users/#{id}", Api.key(token))
     assert response.body == ""
     assert response.status_code == 204
   end
 
   test "cannot delete other user", %{other: %{id: id}} do
-    {:ok, %{body: %{"access_token" => token}}} = login_user()
-    {:ok, %{body: %{"errors" => %{"detail" => message}}}} =
-      Api.delete("/users/#{id}", [{"Authorization", token}])
+    %{"access_token" => token} = login_user()
+    %{"errors" => %{"detail" => message}} = Api.delete!("/users/#{id}", Api.key(token)).body
     assert message =~ "are not authorized"
   end
 
