1d3d0003492bfc19fca090b7a37c1be723b4424a
David Whitlock <alovedalongthe@gmail.com>
2017-09-14 12:55:44 +0700

    Updated todo integration test

--- test/integration/todo_integration_test.exs
+++ test/integration/todo_integration_test.exs
@@ -3,21 +3,41 @@ defmodule TodoAppWeb.TodoIntegrationTest do
 
   import WuffWuff.Api
   import TodoAppWeb.AuthCase
+  alias TodoApp.Jobs
+
+  @todo_attrs %{body: "Need to find the meaning of life",
+    notes: "Have to finish by next Wednesday",
+    title: "Search for meaning"}
 
   setup do
     user = add_user("ted@mail.com")
     other = add_user("frank@mail.com")
-    %{"access_token" => token} = login_user("ted@mail.com")
+    %{"access_token" => token} = login_user(:email, "ted@mail.com")
     {:ok, %{user: user, other: other, token: token}}
   end
 
+  def add_todo(user, attrs) do
+    {:ok, todo} = Jobs.create_todo(user, attrs)
+    todo
+  end
+
   test "can create todo for self", %{user: %{id: id}, token: token} do
-    todo = %{body: "Need to find the meaning of life",
-      notes: "Have to finish by next Wednesday",
-      title: "Search for meaning"}
-    %{"data" => data} = post!("/users/#{id}/todos", %{todo: todo}, ~a(#{token})).body
+    %{"data" => data} = post!("/users/#{id}/todos", %{todo: @todo_attrs}, ~a(#{token})).body
     assert data["body"] =~ "meaning of life"
     assert data["notes"] =~ "finish by next Wednesday"
+  end
+
+  test "can view own todo", %{user: user, token: token} do
+    todo = add_todo(user, @todo_attrs)
+    %{"data" => data} = get!("/users/#{user.id}/todos/#{todo.id}", ~a(#{token})).body
+    assert data["body"] =~ "meaning of life"
+    assert data["notes"] =~ "finish by next Wednesday"
+  end
+
+  test "cannot view other todo", %{other: other, token: token} do
+    todo = add_todo(other, @todo_attrs)
+    %{"errors" => %{"detail" => message}} = get!("/users/#{other.id}/todos/#{todo.id}", ~a(#{token})).body
+    assert message =~ "not authorized to view this resource"
   end
 
 end
